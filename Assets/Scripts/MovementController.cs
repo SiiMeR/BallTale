a60688a3d4e097b293c254b2807d4ceac8fb7466
siimer <siimraudsep@gmail.com>
2018-02-24 19:37:42 +0200

    Added test scene, ball, movement and bouncyness

---
+++ Assets/Scripts/MovementController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementController : MonoBehaviour
+{
+	private Rigidbody2D _rigidbody;
+
+	[SerializeField] private float _movespeed;
+	[SerializeField] private float _jumpforce;
+	
+	// Use this for initialization
+	void Start ()
+	{
+		_rigidbody = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		MovePlayer();
+	}
+
+	private void MovePlayer()
+	{
+		float moveX = Input.GetAxis("Horizontal");
+		float moveY = Input.GetAxis("Vertical");
+
+		if (Input.GetKeyDown(KeyCode.Space))
+		{
+			_rigidbody.AddForce(new Vector2(0, _jumpforce), ForceMode2D.Impulse);
+		}
+		//_rigidbody2D.velocity = new Vector2(moveX * _movespeed, moveY * _movespeed);
+		_rigidbody.velocity = new Vector2(moveX * _movespeed, _rigidbody.velocity.y);
+	}
+}
