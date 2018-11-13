281292924aee47459a90888c251608eb302df4d2
Siim Raudsepp <siimraudsepp@msn.com>
2018-11-13 20:58:30 +0200

    Add dirt bg and fg + dirt tile autofiller

---
+++ Assets/Editor/FillDirtTilesEditor.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(FillDirtTiles))]
+public class FillDirtTilesEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+
+        var fillDirtTiles = (FillDirtTiles) target;
+
+        if (GUILayout.Button("Fill world with dirt!"))
+        {
+            fillDirtTiles.FillWithDirt(Vector3Int.left);
+            fillDirtTiles.FillWithDirt(Vector3Int.right);
+            fillDirtTiles.FillWithDirt(Vector3Int.up);
+            fillDirtTiles.FillWithDirt(Vector3Int.down);
+        } 
+        if (GUILayout.Button("Fill right side of tiles with dirt"))
+        {
+            fillDirtTiles.FillWithDirt(Vector3Int.right);
+        }
+        if (GUILayout.Button("Fill left side of tiles with dirt"))
+        {
+            fillDirtTiles.FillWithDirt(Vector3Int.left);
+        }
+        if (GUILayout.Button("Fill upper side of tiles with dirt"))
+        {
+            fillDirtTiles.FillWithDirt(Vector3Int.up);
+        }
+        if (GUILayout.Button("Fill under side of tiles with dirt"))
+        {
+            fillDirtTiles.FillWithDirt(Vector3Int.down);
+        }  
+        if (GUILayout.Button("Remove all dirt"))
+        {
+            fillDirtTiles.RemoveAllDirt();
+        }
+    }
+}
---
+++ Assets/Scripts/Fillers/FillDirtTiles.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+
+public class FillDirtTiles : MonoBehaviour
+{
+
+    public Tilemap MainTilemap;
+    public Tilemap FGTilemap;
+
+    public Tile Tile;
+    public Tile Underdirt;
+    public Tile Overdirt;
+    public Tile SidedirtRight;
+    public Tile SidedirtLeft;
+
+#if UNITY_EDITOR
+    
+    public void FillWithDirt(Vector3Int direction)
+    {
+        var tileType = direction == Vector3Int.down ? Underdirt    :
+                       direction == Vector3Int.up   ? Overdirt     :
+                       direction == Vector3Int.left ? SidedirtRight:
+                                                      SidedirtLeft ;
+        
+        
+        var positionsInDirection = MainTilemap.GetEmptyAdjacentTilesInDirection<Tile>(direction);
+        
+        foreach (var position in positionsInDirection)
+        {
+            var tilePos =  position + direction;
+            
+            var tile = MainTilemap.GetTile(position);
+
+            if (tile && tile.name.Equals(Tile.name))
+            {    
+                FGTilemap.SetTile(tilePos, tileType);
+            }
+        }
+    }
+    
+    public void RemoveAllDirt()
+    {
+        FGTilemap.RemoveTileOfTypeFromTilemap<Tile>(Underdirt);
+        FGTilemap.RemoveTileOfTypeFromTilemap<Tile>(Overdirt);
+        FGTilemap.RemoveTileOfTypeFromTilemap<Tile>(SidedirtLeft);
+        FGTilemap.RemoveTileOfTypeFromTilemap<Tile>(SidedirtRight);
+    }
+    
+#endif
+
+
+}
