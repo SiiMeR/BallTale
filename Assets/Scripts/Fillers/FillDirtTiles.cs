378e546e424ef83976140cfba02c89b2f496565e
Siim Raudsepp <siimraudsepp@msn.com>
2018-11-22 00:09:19 +0200

    Add automatic dirt filler (doesn't work)

--- Assets/Scripts/Fillers/FillDirtTiles.cs
+++ Assets/Scripts/Fillers/FillDirtTiles.cs
@@ -27,47 +27,93 @@ public class FillDirtTiles : MonoBehaviour
 
 #if UNITY_EDITOR
 
+    /// <summary>
+    /// Fill all nearby tiles with crust tiles depending if they are empty
+    /// </summary>
+    /// TODO: CHECK EVERY EMPTY BLOCK FOR NEARBY DIRT BLOCKS, NOT THE OTHER WAY AROUND. THIS DOESNT WORK lol
     public void FillAllWithDirt()
     {
         var allPositions = MainTilemap.GetEmptyAdjacentTilesInAllDirections<Tile>();
 
         foreach (var (position, nearbySum) in allPositions.Select(x => (x.Key, x.Value)))
         {
+            var tile = ScriptableObject.CreateInstance<Tile>();
+            var tilePos = Vector3Int.zero;
+            
             switch (nearbySum)
             {
                 case 0: // no empty
-                    break;
+                    continue;
                 case 1: // down only
+                    tile = Underdirt;
+                    tilePos = Vector3Int.down;
                     break;
                 case 2: // left only
+                    tile = SidedirtLeft;
+                    tilePos = Vector3Int.left;
                     break;
                 case 3: // d + l
+                    tile = SWDirt;
+                    tilePos = Vector3Int.down + Vector3Int.left;
                     break;
                 case 4: // up only
+                    tile = Overdirt;
+                    tilePos = Vector3Int.up;
                     break;
                 case 5: // u + d
+                    tile = Underdirt;
+                    tilePos = Vector3Int.up; 
                     break;
                 case 6: // u + l
+                    tile = NWDirt;
+                    tilePos = Vector3Int.up + Vector3Int.left;
                     break;
                 case 7: // u + d + l
+                    tile = Underdirt;
+                    tilePos = Vector3Int.down;
                     break;
                 case 8: // right only
+                    tile = SidedirtRight;
+                    tilePos = Vector3Int.right;
                     break;
                 case 9: // r + d
+                    tile = SEDirt;
+                    tilePos = Vector3Int.right + Vector3Int.down;
                     break;
                 case 10: // r + l
+                    tile = Underdirt;
+                    tilePos = Vector3Int.down;
                     break;
                 case 11: // r + d + l
+                    tile = Underdirt;
+                    tilePos = Vector3Int.down;
                     break;
                 case 12: // r + u
+                    tile = NEDirt;
+                    tilePos = Vector3Int.right + Vector3Int.up;
                     break;
                 case 13: // r + u + d
+                    tile = Underdirt;
+                    tilePos = Vector3Int.down;
                     break;
                 case 14: // r + u + l
+                    tile = Underdirt;
+                    tilePos = Vector3Int.down;
                     break;
                 case 15: // r + u + d + l
+                    tile = Underdirt;
+                    tilePos = Vector3Int.down;
                     break;
+            }
 
+
+            tilePos += position;
+            
+            var tilemapTile = MainTilemap.GetTile(position);
+
+            if (tilemapTile && tilemapTile.name.Equals(Tile.name))
+            {    
+                FGTilemap.SetTile(tilePos, tile);
             }
         }
 
