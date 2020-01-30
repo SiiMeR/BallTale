660b81db0115fabdde02580332da77a01fbbb59b
SiiMeR <siimraudsepp@msn.com>
2020-01-30 14:29:50 +0200

    Revert most of last commit

--- Assets/Scripts/Extensions/TupleExtensions.cs
+++ Assets/Scripts/Extensions/TupleExtensions.cs
@@ -5,7 +5,7 @@ namespace Extensions
     public static class TupleExtensions
     {
         public static Vector2 ToVector2(this (float x, float y) tuple) => new Vector2(tuple.x, tuple.y);
-        
+
         public static Vector2Int ToVector2Int(this (int x, int y) tuple) => new Vector2Int(tuple.x, tuple.y);
     }
 }
